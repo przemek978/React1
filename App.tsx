9b77a6a0095528149069ed3ac707648a34edc4a5
przemek978 <78374076+przemek978@users.noreply.github.com>
2022-11-29 22:58:48 +0100

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import ContactList from './ContactList';
-import './style.css';
 
 export default function App() {
   return (
     <div>
-      <ContactList/>
+      <ContactList />
     </div>
   );
 }
